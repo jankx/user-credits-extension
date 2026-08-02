1c1625ca66751bc82f8a838530203b853f5d9341
Puleeno Nguyen <puleeno@gmail.com>
2026-08-02 22:32:51 +0700

    chore: initialize composer autoloader for user-credits extension

---
+++ libs/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'jankx/user-credits',
+        'pretty_version' => 'dev-master',
+        'version' => 'dev-master',
+        'reference' => '2905826cd85da67bf321a0ee23d9dd18df63ad7e',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'jankx/user-credits' => array(
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'reference' => '2905826cd85da67bf321a0ee23d9dd18df63ad7e',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
