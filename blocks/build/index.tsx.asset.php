3c5d3da55d078d30240cefeebf3a49e0a2a42d9e
Puleeno Nguyen <puleeno@gmail.com>
2026-09-03 04:47:07 +0700

    fix: update asset version for user credits block

--- blocks/build/index.tsx.asset.php
+++ blocks/build/index.tsx.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-server-side-render'), 'version' => 'edec25f0ccf7ebfd7445');
+<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-server-side-render'), 'version' => 'cb90d27f4368eac03cfa');
